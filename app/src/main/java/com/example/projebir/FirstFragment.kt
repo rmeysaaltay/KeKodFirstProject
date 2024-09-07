1c92f7a397b3ad9701206f8599f95c8c70df7ca4
rmeysaaltay <altayrumeysa9@gmail.com>
2024-09-07 12:31:42 +0300

    code edited

--- app/src/main/java/com/example/projebir/FirstFragment.kt
+++ app/src/main/java/com/example/projebir/FirstFragment.kt
@@ -19,7 +19,12 @@ class FirstFragment : Fragment() {
 
     private var _binding: FragmentFirstBinding? = null
     private val binding get() = _binding!!
-
+    val ego = binding.ego
+    val giving = binding.giving
+    val respect = binding.respect
+    val happines = binding.happines
+    val kindness = binding.kindness
+    val optimism = binding.optimism
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -47,22 +52,22 @@ class FirstFragment : Fragment() {
 
     fun ego() {
 
-        binding.ego.setOnCheckedChangeListener { _, isChecked ->
+        ego.setOnCheckedChangeListener { _, isChecked ->
             if (isChecked) {
                 bottomNavView.visibility = View.GONE
-                binding.giving.isChecked = false
-                binding.happines.isChecked = false
-                binding.optimism.isChecked = false
-                binding.kindness.isChecked = false
-                binding.respect.isChecked = false
+                giving.isChecked = false
+                happines.isChecked = false
+                optimism.isChecked = false
+                kindness.isChecked = false
+                respect.isChecked = false
 
             } else {
                 bottomNavView.visibility = View.VISIBLE
-                binding.respect.isEnabled = true
-                binding.happines.isEnabled = true
-                binding.optimism.isEnabled = true
-                binding.kindness.isEnabled = true
-                binding.giving.isEnabled = true
+                respect.isEnabled = true
+                happines.isEnabled = true
+                optimism.isEnabled = true
+                kindness.isEnabled = true
+                giving.isEnabled = true
 
             }
         }
@@ -70,20 +75,25 @@ class FirstFragment : Fragment() {
 
 
     fun giving() {
-        if (binding.ego.isChecked) binding.giving.isEnabled = false
+        if (ego.isChecked) giving.isEnabled = false
         val menu = bottomNavView.menu
-        binding.giving.setOnCheckedChangeListener() { _, isChecked ->
+        giving.setOnCheckedChangeListener() { _, isChecked ->
 
-            if (isChecked ) {
+            if (isChecked) {
 
                 if (bottomNavView.menu.size() < 5) {
-                if (menu.findItem(R.id.givingFragment) == null) {
-                    menu.add(Menu.NONE, R.id.givingFragment, Menu.NONE, "giving")
-                        .setIcon(R.drawable.giving_24)
-                } else{
-                    Toast.makeText(requireContext(),"5'den fazla item eklenemez!",Toast.LENGTH_SHORT).show()
+                    if (menu.findItem(R.id.givingFragment) == null) {
+                        menu.add(Menu.NONE, R.id.givingFragment, Menu.NONE, "giving")
+                            .setIcon(R.drawable.giving_24)
+                    } else {
+                        Toast.makeText(
+                            requireContext(),
+                            "5'den fazla item eklenemez!",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
                 }
-            } }else {
+            } else {
                 val item = menu.findItem(R.id.givingFragment)
                 if (item != null) menu.removeItem(R.id.givingFragment)
 
@@ -95,16 +105,20 @@ class FirstFragment : Fragment() {
 
 
     fun happiness() {
-        if (binding.ego.isChecked) binding.happines.isEnabled = false
+        if (ego.isChecked) happines.isEnabled = false
         val menu = bottomNavView.menu
-        binding.happines.setOnCheckedChangeListener() { _, isChecked ->
+        happines.setOnCheckedChangeListener() { _, isChecked ->
             if (isChecked) {
                 if (bottomNavView.menu.size() < 5) {
                     if (menu.findItem(R.id.happinesFragment) == null) {
                         menu.add(Menu.NONE, R.id.happinesFragment, Menu.NONE, "happiness")
                             .setIcon(R.drawable.happy_24)
-                    }else{
-                        Toast.makeText(requireContext(),"5'den fazla item eklenemez!",Toast.LENGTH_SHORT).show()
+                    } else {
+                        Toast.makeText(
+                            requireContext(),
+                            "5'den fazla item eklenemez!",
+                            Toast.LENGTH_SHORT
+                        ).show()
                     }
 
                 }
@@ -119,19 +133,24 @@ class FirstFragment : Fragment() {
 
 
     fun kindness() {
-        if (binding.ego.isChecked) binding.kindness.isEnabled = false
+        if (ego.isChecked) kindness.isEnabled = false
         val menu = bottomNavView.menu
-        binding.kindness.setOnCheckedChangeListener() { _, isChecked ->
-            if (isChecked ) {
+        kindness.setOnCheckedChangeListener() { _, isChecked ->
+            if (isChecked) {
 
                 if (bottomNavView.menu.size() < 5) {
-                if (menu.findItem(R.id.kindnessFragment) == null) {
-                    menu.add(Menu.NONE, R.id.kindnessFragment, Menu.NONE, "kindness")
-                        .setIcon(R.drawable.kind_24)
-                }else{
-                    Toast.makeText(requireContext(),"5'den fazla item eklenemez!",Toast.LENGTH_SHORT).show()
+                    if (menu.findItem(R.id.kindnessFragment) == null) {
+                        menu.add(Menu.NONE, R.id.kindnessFragment, Menu.NONE, "kindness")
+                            .setIcon(R.drawable.kind_24)
+                    } else {
+                        Toast.makeText(
+                            requireContext(),
+                            "5'den fazla item eklenemez!",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
                 }
-            } }else {
+            } else {
                 val item = menu.findItem(R.id.kindnessFragment)
                 if (item != null) menu.removeItem(R.id.kindnessFragment)
 
@@ -141,21 +160,26 @@ class FirstFragment : Fragment() {
 
 
     fun optimism() {
-        if (binding.ego.isChecked) binding.optimism.isEnabled = false
+        if (ego.isChecked) optimism.isEnabled = false
         val menu = bottomNavView.menu
-        binding.optimism.setOnCheckedChangeListener() { _, isChecked ->
-            if (isChecked ) {
+        optimism.setOnCheckedChangeListener() { _, isChecked ->
+            if (isChecked) {
 
                 if (bottomNavView.menu.size() < 5) {
-                val menu = bottomNavView.menu
-                if (menu.findItem(R.id.optimismFragment) == null) {
-                    menu.add(Menu.NONE, R.id.optimismFragment, Menu.NONE, "optimism")
-                        .setIcon(R.drawable.optimism_24)
+                    val menu = bottomNavView.menu
+                    if (menu.findItem(R.id.optimismFragment) == null) {
+                        menu.add(Menu.NONE, R.id.optimismFragment, Menu.NONE, "optimism")
+                            .setIcon(R.drawable.optimism_24)
 
-                }else{
-                    Toast.makeText(requireContext(),"5'den fazla item eklenemez!",Toast.LENGTH_SHORT).show()
+                    } else {
+                        Toast.makeText(
+                            requireContext(),
+                            "5'den fazla item eklenemez!",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
                 }
-            } }else {
+            } else {
                 val item = menu.findItem(R.id.optimismFragment)
                 if (item != null) menu.removeItem(R.id.optimismFragment)
 
@@ -165,22 +189,27 @@ class FirstFragment : Fragment() {
     }
 
     fun respect() {
-        if (binding.ego.isChecked) binding.respect.isEnabled = false
-        binding.respect.setOnCheckedChangeListener() { _, isChecked ->
+        if (ego.isChecked) respect.isEnabled = false
+        respect.setOnCheckedChangeListener() { _, isChecked ->
             val menu = bottomNavView.menu
 
-            if (isChecked ) {
+            if (isChecked) {
 
                 if (bottomNavView.menu.size() < 5) {
 
-                if (menu.findItem(R.id.respectFragment) == null) {
-                    menu.add(Menu.NONE, R.id.respectFragment, Menu.NONE, "respect")
-                        .setIcon(R.drawable.respect_24)
+                    if (menu.findItem(R.id.respectFragment) == null) {
+                        menu.add(Menu.NONE, R.id.respectFragment, Menu.NONE, "respect")
+                            .setIcon(R.drawable.respect_24)
 
-                }else{
-                    Toast.makeText(requireContext(),"5'den fazla item eklenemez!",Toast.LENGTH_SHORT).show()
+                    } else {
+                        Toast.makeText(
+                            requireContext(),
+                            "5'den fazla item eklenemez!",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
                 }
-            } }else {
+            } else {
                 val item = menu.findItem(R.id.respectFragment)
                 if (item != null) {
                     menu.removeItem(R.id.respectFragment)
